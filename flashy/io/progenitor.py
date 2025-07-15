6e9b3e7cba006e6ac9aae84e524b752c99f082cb
Gaétan Jalin <gaetanjalin@gmail.com>
2025-07-15 12:12:07 +0200

    Refactor a few things (hopefully nothing breaks)

--- flashy/io/progenitor.py
+++ flashy/io/progenitor.py
@@ -1,76 +1,121 @@
+from typing import Callable
 import numpy as np
+import re
 
 
 class Progenitor(object):
     """
-    Store FLASH progenitor data.
+    Store progenitor data.
 
     This class can read progenitor profiles
-    from a FLASH progenitor file, or take data from memory.
+    from a FLASH progenitor file, take data from memory,
+    or parse progenitor files from other codes (mesa, kepler, ...).
     The profiles of each variable can then be used,
-    and the progenitor can be saved to a file.
+    and the progenitor can be saved to a FLASH file.
     """
 
     # Private fields
     __source_file: str
     __comment: str
-    __columns: np.ndarray
+    __field_list: list[str]
     __data: np.ndarray
     __loaded: bool
 
     # Constructor
-    def __init__(self, filename = None, parser = 'flash', include_vars = list(), exclude_vars = list()):
+    def __init__(self):
         self.__comment = ''
-        self.__columns = np.empty(0)
-        self.__data = np.empty(0)
+        self.__field_list = None
+        self.__data = None
         self.__loaded = False
-
-        if filename is not None:
-            self.read_file(filename, parser, include_vars, exclude_vars)
 
     # Alternative constructors
     @classmethod
-    def from_file(cls, filename, parser = 'flash', include_vars = list(), exclude_vars = list()):
+    def from_file(cls, file: str, parser: str | Callable = 'flash', include_fields: list[str] = None, exclude_fields: list[str] = None):
         """
         Initialise progenitor object from a progenitor profile
+
+        Parameters
+        ----------
+        file : str
+            Path to the progenitor file.
+        parser : str | Callable
+            By default, the FLASH format for progenitor files
+            is assumed. This variable can be use to force the use of
+            another parser, by passing a Callable
+            object which signature must be equivalent to
+            - custom_parser(file: str) -> (str, dict[str, list[float]])
+            Alternatively, the following string values can be used
+            - 'flash' or 'default': default flash parser
+            - 'mesa': parser for MESA log profiles
+            - 'kepler': parser for KEPLER profiles
+            Caution: mesa format is roughly the same for any version,
+            but kepler files tend to be formatted differently.
+        include_fields : list[str]
+            A list of required variable names.
+            All other variables are discarded.
+        exclude_fields : list[str]
+            A list of variable names from which the associated
+            data must not be stored.
         """
-        prog = cls()
-        prog.read_file(filename, parser, include_vars, exclude_vars)
-        return prog
+        obj = cls()
+        obj.read_file(file, parser, include_fields, exclude_fields)
+        return obj
 
     @classmethod
-    def from_data(cls, r, data, comment: str = ""):
+    def from_data(cls, r: np.ndarray, data: dict[str, np.ndarray], comment: str = ""):
         """
         Initialise progenitor object from memory
+
+        Parameters
+        ----------
+        r :
+            Mid-cell radii of the progenitor profile in increasing order.
+        data :
+            Dictionary with the name of the relevant variables as keys
+            and their values at each given cell radius.
+        comment : str
+            An additional comment to be used when saving to a file.
         """
-        prog = cls()
-        prog.set_data(r, data, comment)
-        return prog
+        obj = cls()
+        obj.set_data(r, data, comment)
+        return obj
 
     def is_loaded(self) -> bool:
         return self.__loaded
 
     def __checkloaded(self) -> None:
         if not self.__loaded:
-            raise RuntimeError('No progenitor has been stored yet!')
+            raise RuntimeError('No progenitor has been loaded yet!')
 
     def __str__(self) -> str:
-        return f'Progenitor @ {self.__source_file}; {self.__comment}; {len(self.__columns)} vars, {self.cells()} cells'
+        return f'Progenitor @ {self.__source_file}; {self.__comment}; {len(self.__field_list)} fields, {self.size()} cells'
 
-    def columns(self):
+    def __len__(self) -> int:
+        return len(self.__data['r'])
+
+    def __contains__(self, key: str):
+        self.__checkloaded()
+        return key in self.__data.dtype.names
+
+    def __bool__(self) -> bool:
+        return self.is_loaded()
+
+    @property
+    def field_list(self):
         """
-        Get the name of the columns.
+        Get the list of available fields
         """
         self.__checkloaded()
-        return self.__columns.copy()
+        return self.__field_list
 
-    def cells(self):
+    def size(self):
         """
         Get the number of cells in the progenitor.
         """
         self.__checkloaded()
         return len(self.__data['r'])
 
+    @property
     def comment(self):
         """
         Get the comment associated with this progenitor
@@ -78,7 +123,8 @@ class Progenitor(object):
         self.__checkloaded()
         return self.__comment
 
-    def set_comment(self, comment: str):
+    @comment.setter
+    def comment(self, comment: str):
         """
         Set the comment associated with this progenitor
         """
@@ -129,29 +175,30 @@ class Progenitor(object):
         else:
             raise IndexError(f'Invalid index type: {type(index)}')
 
-    def read_file(self, filename, parser = 'flash', include_vars = list(), exclude_vars = list()) -> None:
+    def read_file(self, file: str, parser: str | Callable = 'flash', include_fields: list[str] = None, exclude_fields: list[str] = None) -> None:
         """
         Read a progenitor file and loads the data in memory.
 
         Parameters
         ----------
-        filename : str
-            The path to the progenitor file.
+        file : str
+            Path to the progenitor file.
         parser : str | Callable
-            Allow the use of custom file parser.
             By default, the FLASH format for progenitor files
             is assumed. This variable can be use to force the use of
             another parser, by passing a Callable
             object which signature must be equivalent to
-            - custom_parser(filename: str) -> (str, dict[str, list[float]])
+            - custom_parser(file: str) -> (str, dict[str, list[float]])
             Alternatively, the following string values can be used
             - 'flash' or 'default': default flash parser
             - 'mesa': parser for MESA log profiles
             - 'kepler': parser for KEPLER profiles
-        include_vars : list[str]
+            Caution: mesa format is roughly the same for any version,
+            but kepler files tend to be formatted differently.
+        include_fields : list[str]
             A list of required variable names.
             All other variables are discarded.
-        exclude_vars : list[str]
+        exclude_fields : list[str]
             A list of variable names from which the associated
             data must not be stored.
 
@@ -161,8 +208,6 @@ class Progenitor(object):
             If parser has invalid or unknown type.
             If a variable in include_vars is missing from the data.
         """
-        from typing import Callable
-
         # Check progenitor file parser
         if isinstance(parser, str):
             parser = parser.lower()
@@ -179,47 +224,51 @@ class Progenitor(object):
         else:
             raise RuntimeError(f'Invalid parser type: {type(parser)}')
 
-        # Check if a variable is in both include_vars and exclude_vars
-        for invar in include_vars:
-            if invar in exclude_vars:
-                raise RuntimeError(f'Ambiguous variable {invar} found in both included and excluded variables')
+        if not isinstance(exclude_fields, list):
+            exclude_fields = []
+        if not isinstance(include_fields, list):
+            include_fields = []
+
+        # Check if a variable is in both include_fields and exclude_fields
+        for field in include_fields:
+            if field in exclude_fields:
+                raise RuntimeError(f'Ambiguous field {field} found in both included and excluded fields')
 
         # Reset state and clear data
         self.clear()
 
         # Call parser and get data
-        self.__comment, data = file_parser(filename)
-        self.__comment = self.__comment.strip()
+        comment, data = file_parser(file)
+        self.__comment = comment.strip()
 
         # Remove unwanted columns
-        for exvar in exclude_vars:
-            data.pop(exvar, None)
+        for field in exclude_fields:
+            data.pop(field, None)
 
         # Check no missing included variables from progenitor file
-        for invar in include_vars:
-            if invar not in data:
-                raise RuntimeError(f'Variable {invar} requested but absent from progenitor file')
+        for field in include_fields:
+            if field not in data:
+                raise RuntimeError(f'Field {field} requested but not in progenitor file')
 
-        if len(include_vars) > 0:
-            include_vars = list(include_vars)
-            if 'r' not in include_vars:
-                include_vars.append('r')
-            for var in list(data.keys()):
-                if var not in include_vars:
-                    data.pop(var, None)
+        if len(include_fields) > 0:
+            if 'r' not in include_fields:
+                include_fields = ['r'] + include_fields
+            for field in list(data.keys()):
+                if field not in include_fields:
+                    data.pop(field, None)
 
         # Save data
-        self.__columns = np.atleast_1d(list(data.keys()))
-        dtype = [(key, float) for key in data.keys()]
+        self.__field_list = list(data.keys())
+        dtype = [(key, float) for key in data]
         self.__data = np.array(list(zip(*data.values())), dtype=dtype)
-        
-        self.__source_file = filename
+
+        self.__source_file = file
         self.__loaded = True
 
-    def set_data(self, r, data, comment: str = "") -> None:
+    def set_data(self, r: np.ndarray, data: dict[str, np.ndarray], comment: str = '') -> None:
         """
         Set the data describing the progenitor.
-        
+
         Set the data for the progenitor from a list of mid-cell radii
         and a dictionary mapping the name of variables to their values
         for each given radius.
@@ -236,21 +285,21 @@ class Progenitor(object):
         """
         self.clear()
 
-        self.__comment = comment
-        self.__columns = np.concatenate((np.atleast_1d('r'), np.atleast_1d(list(data.keys()))))
-        dtype = [(key, float) for key in self.__columns]
+        self.__comment = comment.strip()
+        self.__field_list = ['r'] + list(data.keys())
+        dtype = [(key, float) for key in self.__field_list]
         self.__data = np.array(list(zip(r, *data.values())), dtype=dtype)
 
-        self.__source_file = 'UNSAVED'
+        self.__source_file = ''
         self.__loaded = True
 
-    def save_file(self, filename):
+    def save_file(self, file: str):
         """
         Save the progenitor profile to a new file.
-    
+
         Parameters
         ----------
-        filename
+        file
             The name of the file where to save the progenitor.
 
         Raises
@@ -260,19 +309,20 @@ class Progenitor(object):
         """
         self.__checkloaded()
         assert 'r' in self.__data.dtype.names, 'Progenitor must have a radial profile!'
-        
-        with open(filename, 'w') as f:
+
+        with open(file, 'w') as f:
             print("#", self.__comment, file=f)
             print("number of variables =", len(self.__data.dtype.names) - 1, file=f)
 
             # Don't print the 'r' var name
-            for var_name in self.__columns[1:]:
-                print(var_name, file=f)
+            for field in self.__field_list:
+                if field != 'r':
+                    print(field, file=f)
     
             for i in range(len(self.__data['r'])):
-                print(*[self.__data[var_name][i] for var_name in self.__columns], file=f)
+                print(*[self.__data[field][i] for field in self.__field_list], file=f)
 
-        self.__source_file = filename
+        self.__source_file = file
 
     def clear(self):
         """
@@ -282,17 +332,17 @@ class Progenitor(object):
         """
         self.__source_file = ''
         self.__comment = ''
-        self.__columns = np.empty(0)
-        self.__data = np.empty(0)
+        self.__field_list = None
+        self.__data = None
         self.__loaded = False
 
 
-def flash_parser(filename):
+def flash_parser(file):
     comment = ''
     data = {}
 
     data_start = 0
-    with open(filename, 'r') as f:
+    with open(file, 'r') as f:
         line = f.readline()
         # Read comment on first line if any
         if (line.startswith('#')):
@@ -309,7 +359,7 @@ def flash_parser(filename):
 
     # Read columns
     array_data = np.genfromtxt(
-        fname=filename,
+        fname=file,
         skip_header=data_start,
         names=var_names,
         dtype=None,
@@ -321,13 +371,13 @@ def flash_parser(filename):
     return comment, data
 
 
-def mesa_parser(filename):
+def mesa_parser(file):
     comment = ''
     data = {}
 
     # Read data after header and column numbers (5 lines)
     array_data = np.genfromtxt(
-        fname=filename,
+        fname=file,
         skip_header=5,
         names=True,
         dtype=None,
@@ -342,5 +392,53 @@ def mesa_parser(filename):
 
     return comment, data
 
-def kepler_parser(filename):
-    raise NotImplementedError('KEPLER parser')
+def kepler_parser(file):
+    """
+    Tested with kepler progenitor files format from Woosley & Heger 2002 and Sukhbold 2018
+    But they are not always the same
+    """
+    comment = ''
+    data = {}
+
+    skip = 2
+    with open(file) as f:
+        line = f.readline()
+        while line.strip() == '':
+            line = f.readline()
+            skip += 1
+        comment = f.readline()
+        line = f.readline()
+        while line.strip() == '':
+            line = f.readline()
+            skip += 1
+        kepler_header = re.split(r'\s{2,}', line)
+        if kepler_header[0] == '':
+            kepler_header = kepler_header[1:]
+        if kepler_header[-1] == '':
+            kepler_header = kepler_header[:-1]
+        while f.readline().strip() == '':
+            skip += 1
+
+    kepler_header = [name.lower() for name in kepler_header]
+
+    raw_data = np.genfromtxt(
+            fname=file,
+            skip_header=skip,
+            dtype=None,
+            missing_values='---',
+            filling_values=0.0,
+            names=kepler_header,
+            usecols=list(range(1, len(kepler_header))),
+            skip_footer=2,
+            encoding='ascii'
+    )
+
+    data['r'] = raw_data['radius']
+    for field in raw_data.dtype.names:
+        if field != 'radius':
+            data[field] = raw_data[field]
+    # mass is cell mass
+    data['mass'] = np.cumsum(data['mass'])
+
+    return comment, data
+
