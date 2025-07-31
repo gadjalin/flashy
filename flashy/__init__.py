8c9baf326f556de7b7e9bdf8a76480f38b984f40
Gaétan Jalin <gaetanjalin@gmail.com>
2025-07-31 14:05:29 +0200

    Move things a bit

--- flashy/__init__.py
+++ flashy/__init__.py
@@ -9,9 +9,7 @@ It's all very extravagant for something you call 'state-of-the-art'...
 A python package for performing various FLASH related tasks.
 
 """
-from . import analysis
 from . import eos
 from . import io
-from . import nuclear
+from . import nuc
 
-from . import plot
